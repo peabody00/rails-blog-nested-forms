b7ea990f2ccc0686fe3cac9c95bbcae90ba263ad
Charlie <peabody00@gmail.com>
2021-04-12 10:00:04 -0500

    commit tree 396b2b9722fd59c11e44364c8a3596bd89542e1d

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,7 @@ class PostsController < ApplicationController
   # GET /posts/new
   def new
     @post = Post.new
+    #@tag = Tag.new
   end
 
   # GET /posts/1/edit
@@ -24,7 +25,9 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
+    #binding.pry
     @post = Post.new(post_params)
+    # binding.pry
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
@@ -68,6 +71,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name, :content, :tag_ids => [])
+      params.require(:post).permit(:name, :content, :tag_ids => [], tags_attributes:[:name])
     end
 end
